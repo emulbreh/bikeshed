46010c66b9473ca5c6a25a2e60fbd1d12685c5c9
Johannes Dollinger <emulbreh@googlemail.com>
2014-04-10 23:51:17 +0200

    Added /api/document/ID/

--- pldm/server/api.py
+++ pldm/server/api.py
@@ -5,6 +5,15 @@ from tornado.web import HTTPError
 from pldm.server.base import BaseHandler
 
 
+def serialize_document(doc):
+    data = doc.serialize()
+    data.update(
+        url='/api/document/%s/' % doc.uid,
+        html_body=doc.html_body(),
+    )
+    return data
+
+
 class DocumentHandler(BaseHandler):
     @property
     def document(self):
@@ -12,6 +21,9 @@ class DocumentHandler(BaseHandler):
             uid = self.path_kwargs.get('uid')
             self._document = self.application.manager.get(uid)
         return self._document
+        
+    def get(self, uid):
+        self.write(serialize_document(self.document))
 
 
 class DocumentsHandler(BaseHandler):
@@ -20,5 +32,5 @@ class DocumentsHandler(BaseHandler):
         result = self.application.manager.search(query=q)
         self.content_type = 'application/json'
         self.write({
-            'documents': [doc.serialize() for doc in result],
+            'documents': [serialize_document(doc) for doc in result],
         })
