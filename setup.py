a3fb01e3bb88dc68ef355c7efb080589e2f1c579
Calin Furau <calin.furau@gmail.com>
2014-04-17 14:58:33 +0200

    Add missing requirements.

--- setup.py
+++ setup.py
@@ -13,10 +13,14 @@ setup(
     long_description=description,
     include_package_data=True,
     install_requires=[
-        'docutils==0.11',
         'elasticsearch==1.0.0',
+        'redis==2.9.1',
+        'docutils==0.11',
         'python-dateutil==2.2',
         'tornado==3.2',
         'Jinja2==2.7.2',
+        'lxml==3.3.4',
+        'couchquery==0.10.1',
+        'httplib2==0.9'
     ],
 )
